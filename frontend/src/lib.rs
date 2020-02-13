483b40677afe0e2dff7eb1b065285973f9022dba
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-12 21:34:59 -0800

    Rustfmt frontend to fix the build

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,10 +1,10 @@
-use seed::{*, prelude::*};
+use seed::{prelude::*, *};
 
 // Model
- 
+
 struct Model {
     count: i32,
-    what_we_count: String
+    what_we_count: String,
 }
 
 // Setup a default here, for initialization later.
@@ -12,11 +12,10 @@ impl Default for Model {
     fn default() -> Self {
         Self {
             count: 0,
-            what_we_count: "click".into()
+            what_we_count: "click".into(),
         }
     }
 }
-
 
 // Update
 
@@ -36,57 +35,64 @@ fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
     }
 }
 
-
 // View
 
 /// A simple component.
 fn success_level(clicks: i32) -> Node<Msg> {
     let descrip = match clicks {
-        0 ..= 5 => "Not very many 🙁",
-        6 ..= 9 => "I got my first real six-string 😐",
-        10 ..= 11 => "Spinal Tap 🙂",
-        _ => "Double pendulum 🙃"
+        0..=5 => "Not very many 🙁",
+        6..=9 => "I got my first real six-string 😐",
+        10..=11 => "Spinal Tap 🙂",
+        _ => "Double pendulum 🙃",
     };
-    p![ descrip ]
+    p![descrip]
 }
 
 /// The top-level component we pass to the virtual dom.
 fn view(model: &Model) -> impl View<Msg> {
-    let plural = if model.count == 1 {""} else {"s"};
+    let plural = if model.count == 1 { "" } else { "s" };
 
     // Attrs, Style, Events, and children may be defined separately.
-    let outer_style = style!{
+    let outer_style = style! {
             St::Display => "flex";
             St::FlexDirection => "column";
             St::TextAlign => "center"
     };
 
-    div![ outer_style,
-        h1![ "The Grand Total" ],
+    div![
+        outer_style,
+        h1!["The Grand Total"],
         div![
-            style!{
+            style! {
                 // Example of conditional logic in a style.
                 St::Color => if model.count > 4 {"purple"} else {"gray"};
-                St::Border => "2px solid #004422"; 
+                St::Border => "2px solid #004422";
                 St::Padding => unit!(20, px);
             },
             // We can use normal Rust code and comments in the view.
-            h3![ format!("{} {}{} so far", model.count, model.what_we_count, plural) ],
-            button![ simple_ev(Ev::Click, Msg::Increment), "+" ],
-            button![ simple_ev(Ev::Click, Msg::Decrement), "-" ],
-
+            h3![format!(
+                "{} {}{} so far",
+                model.count, model.what_we_count, plural
+            )],
+            button![simple_ev(Ev::Click, Msg::Increment), "+"],
+            button![simple_ev(Ev::Click, Msg::Decrement), "-"],
             // Optionally-displaying an element
-            if model.count >= 10 { h2![ style!{St::Padding => px(50)}, "Nice!" ] } else { empty![] }
+            if model.count >= 10 {
+                h2![style! {St::Padding => px(50)}, "Nice!"]
+            } else {
+                empty![]
+            }
         ],
-        success_level(model.count),  // Incorporating a separate component
-
-        h3![ "What are we counting eh2?" ],
-        input![ attrs!{At::Value => model.what_we_count}, input_ev(Ev::Input, Msg::ChangeWWC) ]
+        success_level(model.count), // Incorporating a separate component
+        h3!["What are we counting eh2?"],
+        input![
+            attrs! {At::Value => model.what_we_count},
+            input_ev(Ev::Input, Msg::ChangeWWC)
+        ]
     ]
 }
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    App::builder(update, view)
-        .build_and_start();
+    App::builder(update, view).build_and_start();
 }
