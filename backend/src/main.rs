09e891ad341f45443c99eede26bf9aec24f5cf64
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-12 21:21:46 -0800

    Add random string to an endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,10 +3,15 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    // GET /meals
+    let all_meals = warp::path("meals").map(|| format!("all the meals {}", all_meals()));
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let routes = warp::get().and(hello.or(all_meals));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn all_meals() -> i32 {
+    rand::random::<i32>()
 }
