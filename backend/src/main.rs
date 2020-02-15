cedf22c311e5b8d73976c4d0e6368415acf1d5ea
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-15 14:40:46 -0800

    Allow local testing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,10 +4,16 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+    let cors = warp::cors()
+        .allow_origin("http://localhost:8000")
+        .allow_origin("http://127.0.0.1:8000")
+        .allow_methods(vec!["GET", "POST", "DELETE"]);
     // GET /hello/warp
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     // GET /meals
-    let all_meals = warp::path("meals").map(|| warp::reply::json(&all_meals()));
+    let all_meals = warp::path("meals")
+        .map(|| warp::reply::json(&all_meals()))
+        .with(cors);
 
     let routes = warp::get().and(hello.or(all_meals));
 
