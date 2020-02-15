95bb7a88148081916a86921e7d83c25de4ce7374
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-15 10:46:22 -0800

    Finally compiles

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,18 +1,18 @@
 use seed::{prelude::*, *};
 use serde::{Serialize, Deserialize};
+use std::iter::*;
+use indexmap::IndexMap;
 
 // Model
 struct Model {
-    count: i32,
-    what_we_count: String,
+    meals: IndexMap::<Meal, u32>,
 }
 
 // Setup a default here, for initialization later.
 impl Default for Model {
     fn default() -> Self {
         Self {
-            count: 0,
-            what_we_count: "click".into(),
+            meals: IndexMap::<Meal, u32>::new()
         }
     }
 }
@@ -20,73 +20,33 @@ impl Default for Model {
 // Update
 #[derive(Clone)]
 enum Msg {
-    Increment,
-    Decrement,
-    ChangeWWC(String),
+    GetAllMeals(IndexMap::<Meal, u32>),
 }
 
 /// How we update the model
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => model.count += 1,
-        Msg::Decrement => model.count -= 1,
-        Msg::ChangeWWC(what_we_count) => model.what_we_count = what_we_count,
+        Msg::GetAllMeals(meals) => model.meals = meals,
     }
 }
 
 // View
 
-/// A simple component.
-fn success_level(clicks: i32) -> Node<Msg> {
-    let descrip = match clicks {
-        0..=5 => "Not very many 🙁",
-        6..=9 => "I got my first real six-string 😐",
-        10..=11 => "Spinal Tap 🙂",
-        _ => "Double pendulum 🙃",
-    };
-    p![descrip]
-}
-
 /// The top-level component we pass to the virtual dom.
 fn view(model: &Model) -> impl View<Msg> {
-    let plural = if model.count == 1 { "" } else { "s" };
-
-    // Attrs, Style, Events, and children may be defined separately.
-    let outer_style = style! {
-            St::Display => "flex";
-            St::FlexDirection => "column";
-            St::TextAlign => "center"
-    };
-
     div![
-        outer_style,
         div![
             style! {
                 // Example of conditional logic in a style.
-                St::Color => if model.count > 4 {"purple"} else {"gray"};
+                St::Color => {"gray"};
                 St::Border => "2px solid #004422";
                 St::Padding => unit!(20, px);
             },
-            // We can use normal Rust code and comments in the view.
-            h3![format!(
-                "{} {}{} so far",
-                model.count, model.what_we_count, plural
-            )],
-            button![simple_ev(Ev::Click, Msg::Increment), "+"],
-            button![simple_ev(Ev::Click, Msg::Decrement), "-"],
-            // Optionally-displaying an element
-            if model.count >= 10 {
-                h2![style! {St::Padding => px(50)}, "Nice!"]
-            } else {
-                empty![]
-            }
+            h3!["Meals available"],
+            model.meals.iter().map(|m| h4![format!("{:?}", m)]),
+            // button![simple_ev(Ev::Click, Msg::GetAllMeals), "get em"],
         ],
-        success_level(model.count), // Incorporating a separate component
         h3!["What are we counting  ?"],
-        input![
-            attrs! {At::Value => model.what_we_count},
-            input_ev(Ev::Input, Msg::ChangeWWC)
-        ]
     ]
 }
 
@@ -105,22 +65,21 @@ async fn fetch_data() -> Msg2 {
     }
 }
 
-fn after_mount(_: Url, orders: &mut impl Orders<Msg2>) -> AfterMount<Model> {
-    orders.perform_cmd(fetch_data());
-    AfterMount::default()
-}
+// fn after_mount(_: Url, orders: &mut impl Orders<Msg2>) -> AfterMount<Model> {
+//     orders.perform_cmd(fetch_data());
+//     AfterMount::default()
+// }
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    App::builder(update, view).after_mount(after_mount).build_and_start();
+    App::builder(update, view).build_and_start();
 }
-
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 struct Meals {
-    meals: Vec<Meal>,
+    meals: IndexMap<Meal, u32>,
 }
-#[derive(Deserialize, Serialize, Clone, Debug)]
+#[derive(Deserialize, Serialize, Clone, Eq, PartialEq, Hash, Debug)]
 struct Meal {
     name: String,
     id: u32,
