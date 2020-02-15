5f9722639d2009f666e456cdd13dca9d3dff93df
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-15 11:24:04 -0800

    Trying to get fetching working

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,7 +1,8 @@
-use seed::{prelude::*, *};
+use seed::{browser::service::fetch, prelude::*, *};
 use serde::{Serialize, Deserialize};
 use std::iter::*;
 use indexmap::IndexMap;
+use futures::Future;
 
 // Model
 struct Model {
@@ -21,12 +22,22 @@ impl Default for Model {
 #[derive(Clone)]
 enum Msg {
     GetAllMeals(IndexMap::<Meal, u32>),
+    DataFetched(seed::fetch::ResponseDataResult<IndexMap::<Meal, u32>>),
+    FetchData,
 }
 
 /// How we update the model
-fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::GetAllMeals(meals) => model.meals = meals,
+        Msg::FetchData => {
+            println!("fetching");
+            orders.skip().perform_cmd(fetch_data());
+        },
+        Msg::DataFetched(Ok(meals)) => {
+            println!("updated");
+            model.meals = meals;
+        },
+        _ => println!("other"),
     }
 }
 
@@ -44,7 +55,7 @@ fn view(model: &Model) -> impl View<Msg> {
             },
             h3!["Meals available"],
             model.meals.iter().map(|m| h4![format!("{:?}", m)]),
-            // button![simple_ev(Ev::Click, Msg::GetAllMeals), "get em"],
+            button![simple_ev(Ev::Click, Msg::FetchData), "get em"],
         ],
         h3!["What are we counting  ?"],
     ]
@@ -52,27 +63,17 @@ fn view(model: &Model) -> impl View<Msg> {
 
 // https://seed-rs.org/guide/http-requests-and-state
 
-#[derive(Clone, Debug)]
-enum Msg2 {
-    DataFetched(seed::fetch::ResponseDataResult<Meals>),
-}
-
-async fn fetch_data() -> Msg2 {
+async fn fetch_data() -> Msg {
     let url = "localhost:3030/meals";
-    match Request::new(url).fetch_json_data(Msg2::DataFetched).await {
-        Ok(i) => return i,
-        Err(e) => return e,
-    }
+    Request::new(url).fetch_json_data(Msg::DataFetched).await
 }
-
-// fn after_mount(_: Url, orders: &mut impl Orders<Msg2>) -> AfterMount<Model> {
-//     orders.perform_cmd(fetch_data());
-//     AfterMount::default()
-// }
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    App::builder(update, view).build_and_start();
+    let app = seed::App::builder(update, view)
+        .build_and_start();
+
+    app.update(Msg::FetchData);
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
