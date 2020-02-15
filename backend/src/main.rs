be35f90edcc7b415537adaff8193fadd5ad5c74d
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-14 22:47:39 -0800

    Return some json

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,35 @@
+use serde_derive::{Deserialize, Serialize};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    pretty_env_logger::init();
+    // GET /hello/warp
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     // GET /meals
-    let all_meals = warp::path("meals").map(|| format!("all the meals {}", all_meals()));
+    let all_meals = warp::path("meals").map(|| warp::reply::json(&all_meals()));
 
     let routes = warp::get().and(hello.or(all_meals));
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-fn all_meals() -> i32 {
-    rand::random::<i32>()
+fn all_meals() -> Meals {
+    let m1 = Meal {
+        name: format!("meal {}", rand::random::<i32>()),
+        id: rand::random::<u32>(),
+    };
+
+    Meals { meals: vec![m1] }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Meals {
+    meals: Vec<Meal>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Meal {
+    name: String,
+    id: u32,
 }
