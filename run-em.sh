86e7a991fd17cbb92db946a0331e21d28a6f5ac7
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-25 18:29:33 -0800

    npx eh

--- run-em.sh
+++ run-em.sh
@@ -2,10 +2,10 @@
 
 # compile things
 (cd backend && cargo build)
-(cd frontend && cargo build && wasm-pack build  --target web --out-name package --dev)
+(cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev)
 
 (cd backend && RUST_LOG="meals" cargo run) &
-(cd frontend && cargo build && wasm-pack build  --target web --out-name package --dev && docker build . -t rrampage:local && docker run --name rrampage -p 8080:9090 rrampage:local) &
+(cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev && docker build . -t rrampage:local && docker run --name rrampage -p 8080:9090 rrampage:local) &
 # wait for services to come up: curl in a loop?
 echo "\n\nwaiting is the hardest part\n\n"
 sleep 5
