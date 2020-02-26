8a9f133259b852b34235246d531ce141be8b49ba
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-25 16:29:48 -0800

    Runem works

--- run-em.sh
+++ run-em.sh
@@ -8,8 +8,11 @@ cargo install microserver
 (cd backend && RUST_LOG="meals" cargo run) &
 (cd frontend && cargo make build frontend && cargo make serve frontend) &
 # wait for services to come up: curl in a loop?
-echo "waiting is the hardest part"
+echo "\n\nwaiting is the hardest part\n\n"
 sleep 5
 (cd gauge-tests && gauge run specs)
 
 # kill remaining cargo processes
+killall cargo-make
+killall backend
+killall microserver
