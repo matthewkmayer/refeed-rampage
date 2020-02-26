281dd811562bf1334fd7d2b60ce2468e0ad82c3d
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-25 19:54:47 -0800

    Install taiko here

--- run-em.sh
+++ run-em.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # compile things
-(cd backend && cargo build)
-(cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev)
+# (cd backend && cargo build)
+# (cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev)
 
-(cd backend && RUST_LOG="meals" cargo run) &
-(cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev && docker build . -t rrampage:local && docker run --name rrampage -p 8080:9090 rrampage:local) &
-# wait for services to come up: curl in a loop?
-echo "\n\nwaiting is the hardest part\n\n"
-sleep 5
+# (cd backend && RUST_LOG="meals" cargo run) &
+# (cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev && docker build . -t rrampage:local && docker run --name rrampage -p 8080:9090 rrampage:local) &
+# # wait for services to come up: curl in a loop?
+# echo "\n\nwaiting is the hardest part\n\n"
+# sleep 5
 
 cd gauge-tests
+npm install taiko
 if R=$(npx gauge run specs); then
     # Success.
     echo "woo r is $R"
