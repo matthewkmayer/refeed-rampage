5deb53e0f3b95e44cd2ded5f263c62c47fa75062
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-26 22:09:55 -0800

    Edit page.

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -438,26 +438,21 @@ fn routes(url: Url) -> Option<Msg> {
                 if page == &"create" {
                     return Some(Msg::ChangePage(Pages::CreateMeal));
                 }
-                if page == &"edit" {
-                    match url.path.get(2).as_ref() {
-                        Some(mid) => match mid.parse::<i32>() {
-                            Ok(i) => {
-                                return Some(Msg::ChangePage(Pages::EditMeal { meal_id: i }));
-                            }
-                            Err(_) => {
-                                return Some(Msg::ChangePage(Pages::Meals { meal_id: None }));
-                            }
-                        },
-                        None => return Some(Msg::ChangePage(Pages::Meals { meal_id: None })),
-                    }
-                }
                 match page.parse::<i32>() {
-                    Ok(m_id) => Msg::ChangePage(Pages::Meals {
-                        meal_id: Some(m_id),
-                    }),
+                    Ok(m_id) => {
+                        match url.path.get(2).as_ref() {
+                            Some(i) => {
+                                if i == &"edit" {
+                                    return Some(Msg::ChangePage(Pages::EditMeal {meal_id: m_id}));
+                                }
+                                return Some(Msg::ChangePage(Pages::Meals {meal_id: Some(m_id)}));
+                            }
+                            None => return Some(Msg::ChangePage(Pages::Meals {meal_id: Some(m_id)})),
+                        }
+                    }
                     Err(e) => {
                         log!("Got an error on meal id: {}", e);
-                        Msg::ChangePage(Pages::Meals { meal_id: None })
+                        return Some(Msg::ChangePage(Pages::Meals { meal_id: None }));
                     }
                 }
             }
