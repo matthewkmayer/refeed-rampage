f91da9c42d858f0a5727f4c676114265daef605a
Matthew Mayer <matthewkmayer@gmail.com>
2020-02-29 20:47:24 -0800

    This should break

--- run-em-for-cukes.sh
+++ run-em-for-cukes.sh
@@ -2,5 +2,5 @@
 
 # compile things
 (cd backend && cargo build)
-docker run --rm -p 8000:8000 amazon/dynamodb-local &
+# docker run --rm -p 8000:8000 amazon/dynamodb-local &
 (cd backend && RUST_LOG="meals" cargo run) &
