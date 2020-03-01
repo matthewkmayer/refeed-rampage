4c867a4842e4a9bbad28468182683dbc3d0a0a01
Matthew Mayer <matthewkmayer@gmail.com>
2020-03-01 11:19:29 -0800

    C'mon GHA

--- run-em-for-cukes.sh
+++ run-em-for-cukes.sh
@@ -3,6 +3,6 @@
 # compile things
 (cd backend && cargo build)
 docker run --rm -p 8000:8000 amazon/dynamodb-local &
-sleep 10
+sleep 12
 docker ps -a
 (cd backend && RUST_LOG="meals" AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY cargo run) &
--- run-em.sh
+++ run-em.sh
@@ -4,7 +4,7 @@
 (cd backend && cargo build)
 (cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev)
 docker run --rm -p 8000:8000 amazon/dynamodb-local &
-sleep 10
+sleep 12
 (cd backend && RUST_LOG="meals" AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY cargo run) &
 (cd frontend && cargo build && npx wasm-pack build  --target web --out-name package --dev && docker build . -t rrampage:local && docker run --name rrampage -p 8080:9090 rrampage:local) &
 
