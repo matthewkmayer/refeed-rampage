3fa88c45246e9d0f51a96139548b9038701ac67f
Matthew Mayer <matthewkmayer@gmail.com>
2020-04-27 18:47:37 -0700

    Add links to breadcrumbs so history works.

--- frontend/src/breadcrumbs.rs
+++ frontend/src/breadcrumbs.rs
@@ -7,12 +7,14 @@ pub fn breadcrumbs(page: &Pages, meal_v: &Meal, meal_edit: &Meal) -> Node<Msg> {
     Pages::Meals => p![
       "meals",
       style! {St::Cursor => "pointer"},
+      attrs! {At::Href => "/meals"},
       simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
     ],
     Pages::ViewSpecificMeal { meal_id: _ } => p![
       span![
         "meals",
         style! {St::Cursor => "pointer"},
+        attrs! {At::Href => "/meals"},
         simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
       ],
       span![" > "],
@@ -22,12 +24,14 @@ pub fn breadcrumbs(page: &Pages, meal_v: &Meal, meal_edit: &Meal) -> Node<Msg> {
       span![
         "meals",
         style! {St::Cursor => "pointer"},
+        attrs! {At::Href => "/meals"},
         simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
       ],
       span![" > "],
       span![
         format!(" {} ", meal_edit.name),
         style! {St::Cursor => "pointer"},
+        attrs! {At::Href => format!("/meals/{}", meal_edit.id)},
         simple_ev(
           Ev::Click,
           Msg::ChangePage(Pages::ViewSpecificMeal {
@@ -42,6 +46,7 @@ pub fn breadcrumbs(page: &Pages, meal_v: &Meal, meal_edit: &Meal) -> Node<Msg> {
       span![
         "meals",
         style! {St::Cursor => "pointer"},
+        attrs! {At::Href => "/meals"},
         simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
       ],
       span![" > "],
