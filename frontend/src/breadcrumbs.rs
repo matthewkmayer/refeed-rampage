17b0005a0ff07c3fde1c4712eaa5887b3a3310b7
Matthew Mayer <matthewkmayer@gmail.com>
2020-04-27 18:48:17 -0700

    Cargo fmt from command line.

--- frontend/src/breadcrumbs.rs
+++ frontend/src/breadcrumbs.rs
@@ -2,56 +2,56 @@ use crate::{Meal, Msg, Pages};
 use seed::{prelude::*, *};
 
 pub fn breadcrumbs(page: &Pages, meal_v: &Meal, meal_edit: &Meal) -> Node<Msg> {
-  match page {
-    Pages::Home => empty!(),
-    Pages::Meals => p![
-      "meals",
-      style! {St::Cursor => "pointer"},
-      attrs! {At::Href => "/meals"},
-      simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
-    ],
-    Pages::ViewSpecificMeal { meal_id: _ } => p![
-      span![
-        "meals",
-        style! {St::Cursor => "pointer"},
-        attrs! {At::Href => "/meals"},
-        simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
-      ],
-      span![" > "],
-      span![format!(" {}", meal_v.name)],
-    ],
-    Pages::EditMeal { meal_id: _ } => p![
-      span![
-        "meals",
-        style! {St::Cursor => "pointer"},
-        attrs! {At::Href => "/meals"},
-        simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
-      ],
-      span![" > "],
-      span![
-        format!(" {} ", meal_edit.name),
-        style! {St::Cursor => "pointer"},
-        attrs! {At::Href => format!("/meals/{}", meal_edit.id)},
-        simple_ev(
-          Ev::Click,
-          Msg::ChangePage(Pages::ViewSpecificMeal {
-            meal_id: meal_edit.id
-          })
-        )
-      ],
-      span![" > "],
-      span!["edit"]
-    ],
-    Pages::CreateMeal => p![
-      span![
-        "meals",
-        style! {St::Cursor => "pointer"},
-        attrs! {At::Href => "/meals"},
-        simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
-      ],
-      span![" > "],
-      span!["create new meal"]
-    ],
-    Pages::Login => empty!(),
-  }
+    match page {
+        Pages::Home => empty!(),
+        Pages::Meals => p![
+            "meals",
+            style! {St::Cursor => "pointer"},
+            attrs! {At::Href => "/meals"},
+            simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
+        ],
+        Pages::ViewSpecificMeal { meal_id: _ } => p![
+            span![
+                "meals",
+                style! {St::Cursor => "pointer"},
+                attrs! {At::Href => "/meals"},
+                simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
+            ],
+            span![" > "],
+            span![format!(" {}", meal_v.name)],
+        ],
+        Pages::EditMeal { meal_id: _ } => p![
+            span![
+                "meals",
+                style! {St::Cursor => "pointer"},
+                attrs! {At::Href => "/meals"},
+                simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
+            ],
+            span![" > "],
+            span![
+                format!(" {} ", meal_edit.name),
+                style! {St::Cursor => "pointer"},
+                attrs! {At::Href => format!("/meals/{}", meal_edit.id)},
+                simple_ev(
+                    Ev::Click,
+                    Msg::ChangePage(Pages::ViewSpecificMeal {
+                        meal_id: meal_edit.id
+                    })
+                )
+            ],
+            span![" > "],
+            span!["edit"]
+        ],
+        Pages::CreateMeal => p![
+            span![
+                "meals",
+                style! {St::Cursor => "pointer"},
+                attrs! {At::Href => "/meals"},
+                simple_ev(Ev::Click, Msg::ChangePage(Pages::Meals))
+            ],
+            span![" > "],
+            span!["create new meal"]
+        ],
+        Pages::Login => empty!(),
+    }
 }
